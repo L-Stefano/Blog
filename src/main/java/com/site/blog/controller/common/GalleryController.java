06ea5c8c034123ed4f008ddbd4c500b8e79ca55b
Stefano <1844231993@qq.com>
2023-05-08 14:17:49 +0800

    作品集：摄影、个人介绍完成；

--- src/main/java/com/site/blog/controller/common/GalleryController.java
+++ src/main/java/com/site/blog/controller/common/GalleryController.java
@@ -4,8 +4,12 @@ import com.site.blog.service.BlogConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class GalleryController
@@ -19,5 +23,20 @@ public class GalleryController
         request.setAttribute("configurations", blogConfigService.getAllConfigs());
         request.setAttribute("pageName", "作品集");
         return "gallery/gallery";
+    }
+
+    @GetMapping("/gallery/photos")
+    @ResponseBody
+    public List<String> getPhotoList() {
+        List<String> photos = new ArrayList<>();
+        String photoDir = "src/main/resources/static/gallery/photos";
+        File[] photoFiles = new File(photoDir).listFiles();
+        for (File photoFile : photoFiles) {
+            String photoName = photoFile.getName();
+            if (photoFile.isFile() && photoName.matches(".*\\.(jpe?g|png|gif)$")) {
+                photos.add(photoName);
+            }
+        }
+        return photos;
     }
 }
--- src/test/java/com/site/blog/MyBlogApplicationTests.java
+++ src/test/java/com/site/blog/MyBlogApplicationTests.java
@@ -1,8 +1,10 @@
 package com.site.blog;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.site.blog.controller.common.GalleryController;
 import com.site.blog.entity.RevisionInfo;
 import com.site.blog.service.RevisionInfoService;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +17,10 @@ public class MyBlogApplicationTests {
 
 	@Autowired
 	RevisionInfoService revisionInfoService;
+
 	@Test
 	public void contextLoads() {
-//		//获取所有版本记录
-//		QueryWrapper<RevisionInfo> queryWrapper = new QueryWrapper<>();
-//		queryWrapper.lambda()
-//				.orderByDesc(RevisionInfo::getRevisionDatetime);
-//		System.out.println(revisionInfoService.list(queryWrapper));
+
 	}
 
 }
